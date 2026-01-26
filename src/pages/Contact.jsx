c4344e9691f50adbb76467ca66b298044ca9e5ca
Mohammed Baba <mbfmz20@gmail.com>
2026-01-26 15:50:37 +0100

    setup Contact page with formik form, validation, and Leaflet map

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 const Contact = () => {
   return (
