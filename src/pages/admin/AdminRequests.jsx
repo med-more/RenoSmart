55a22d3cb0dc22182ecad77c476b705b00ca96bb
Mohammed Baba <mbfmz20@gmail.com>
2026-01-29 17:13:33 +0100

    feat(admin-requests): create placeholder component for AdminRequests page

--- src/pages/admin/AdminRequests.jsx
+++ src/pages/admin/AdminRequests.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { getAllRenovationRequests } from '../../store/slices/renovationSlice';
+import { STATUS_LABELS, STATUS_COLORS } from '../../utils/constants';
+
 
 const AdminRequests = () => {
   return (
