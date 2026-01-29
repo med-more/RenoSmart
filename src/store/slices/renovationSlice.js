21caa28b28a2b21786f500b36a42195d1485691b
Mohammed Baba <mbfmz20@gmail.com>
2026-01-29 14:33:53 +0100

    feat(renovation): add async thunk to create renovation request

--- src/store/slices/renovationSlice.js
+++ src/store/slices/renovationSlice.js
@@ -7,3 +7,15 @@ import {
   updateRenovationRequest,
   notifyStatusChange,
 } from '../../services/renovationService';
+
+export const addRenovationRequest = createAsyncThunk(
+  'renovation/addRequest',
+  async (requestData, { rejectWithValue }) => {
+    try {
+      const response = await createRenovationRequest(requestData);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
