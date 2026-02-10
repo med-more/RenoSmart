6c52e43cb3e3c1c294b36b2667dcdbdabe3f9e6b
med-more <mbfmz20@gmail.com>
2026-02-10 16:19:41 +0100

    feat: initialize RenovationDetails page with Redux and routing setup

--- src/pages/admin/RenovationDetails.jsx
+++ src/pages/admin/RenovationDetails.jsx
@@ -1,4 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, useNavigate, Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import toast from 'react-hot-toast';
+import {
+  getAllRenovationRequests,
+  updateRequestStatus,
+  updateRequestInternalNotes,
+  updateRequestPDF,
+  removeRenovationRequest,
+  setCurrentRequest,
+} from '../../store/slices/renovationSlice';
+import { setSuccessMessage, clearMessages, setLoading, setError } from '../../store/slices/uiSlice';
+import { REQUEST_STATUSES, STATUS_LABELS, STATUS_COLORS } from '../../utils/constants';
+import { sendRequestDetailsEmail } from '../../services/renovationService';
+
 
 const RenovationDetails = () => {
   return (
