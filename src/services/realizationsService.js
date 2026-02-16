fb14768fd87685adce23f458a33a175d09ed5143
med-more <mbfmz20@gmail.com>
2026-02-16 20:24:17 +0000

    add fetchRealizations API function

--- src/services/realizationsService.js
+++ src/services/realizationsService.js
@@ -11,3 +11,12 @@ export const createRealization = async (projectData) => {
     throw new Error(`Erreur lors de la création du projet: ${error.message}`);
   }
 };
+
+export const fetchRealizations = async () => {
+  try {
+    const response = await realizationsApi.get('');
+    return response.data;
+  } catch (error) {
+    console.warn('Impossible de récupérer les projets depuis l\'API:', error.message);
+  }
+};
