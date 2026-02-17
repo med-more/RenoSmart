0669117245682b448ed33b2177e37283224eb170
med-more <mbfmz20@gmail.com>
2026-02-17 13:52:56 +0000

    add EditProjectModal component skeleton

---
+++ src/components/admin/EditProjectModal.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import toast from 'react-hot-toast';
+import { fetchRealizationById, updateRealization } from '../../services/realizationsService';
+
+
+const EditProjectModal = () => {
+  return (
+    <div>EditProjectModal</div>
+  )
+}
+
+export default EditProjectModal
